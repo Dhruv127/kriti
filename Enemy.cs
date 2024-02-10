6d8ed9d88f32e174fdced824c4ccb80d799ed16c
Dhruv Agarwal <92450489+Dhruv127@users.noreply.github.com>
2024-02-10 20:12:20 +0530

    Update Enemy.cs

--- Enemy.cs
+++ Enemy.cs
@@ -1,10 +1,9 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    // Start is called before the first frame update
 
 
     private Vector2 movement;
@@ -20,6 +19,7 @@ public class Enemy : MonoBehaviour
     public float footOffset = 1;
     public LayerMask groundLayer;
     bool triger=false;
+    
 
 
 
@@ -30,7 +30,6 @@ public class Enemy : MonoBehaviour
         playerHealth=GetComponent<PlayerHealth>();
     }
 
-    // Update is called once per frame
     void Update()
     {
 
@@ -47,21 +46,8 @@ public class Enemy : MonoBehaviour
 
     void FixedUpdate()
     {
-        //Cast rays for the left and right foot
-		RaycastHit2D leftCheck = Raycast(new Vector2(-footOffset, 0f), Vector2.down, groundDistance);
-		RaycastHit2D rightCheck = Raycast(new Vector2(footOffset, 0f), Vector2.down, groundDistance);
-
-		//If either ray hit the ground, the player is on the ground
-		if (!leftCheck && !rightCheck)
-        {
-			isOnGround = false;
-            // Instantiate(playerHealth.deathVFXPrefab, transform.position, transform.rotation);
-
-            //Disable player game object
-            gameObject.SetActive(false);
-            Debug.Log("hi");
-        }
-        else if(torch.enabled&&((Mathf.Abs(player.position.x-transform.position.x)<=range&&Mathf.Abs(player.position.y-transform.position.y)<=0.5f)||triger))
+        
+        if(torch.enabled&&((Mathf.Abs(player.position.x-transform.position.x)<=range&&Mathf.Abs(player.position.y-transform.position.y)<=0.5f)||triger))
         {
         moveCharacter(movement);  
         triger=true;
@@ -80,29 +66,16 @@ public class Enemy : MonoBehaviour
 
     RaycastHit2D Raycast(Vector2 offset, Vector2 rayDirection, float length)
 	{
-		//Call the overloaded Raycast() method using the ground layermask and return 
-		//the results
 		return Raycast(offset, rayDirection, length, groundLayer);
 	}
 
 	RaycastHit2D Raycast(Vector2 offset, Vector2 rayDirection, float length, LayerMask mask)
 	{
-		//Record the player's position
 		Vector2 pos = transform.position;
 
-		//Send out the desired raycasr and record the result
 		RaycastHit2D hit = Physics2D.Raycast(pos + offset, rayDirection, length, mask);
 
-		// //If we want to show debug raycasts in the scene...
-		// if (drawDebugRaycasts)
-		// {
-		// 	//...determine the color based on if the raycast hit...
-		// 	Color color = hit ? Color.red : Color.green;
-		// 	//...and draw the ray in the scene view
-		// 	Debug.DrawRay(pos + offset, rayDirection * length, color);
-		// }
 
-		//Return the results of the raycast
 		return hit;
 	}
 
