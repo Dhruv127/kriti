e80f1d90ba88c9408c508f1609db4ecfd3edb1a3
Dhruv Agarwal <92450489+Dhruv127@users.noreply.github.com>
2024-02-10 20:25:45 +0530

    Add files via upload

---
+++ Door.cs
@@ -0,0 +1,24 @@
+﻿
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+	Animator anim;			
+	int openParameterID;	
+
+
+	void Start()
+	{
+		anim = GetComponent<Animator>();
+
+		openParameterID = Animator.StringToHash("Open");
+
+		GameManager.RegisterDoor(this);
+	}
+
+	public void Open()
+	{
+		anim.SetTrigger(openParameterID);
+		AudioManager.PlayDoorOpenAudio();
+	}
+}
---
+++ UIManager.cs
@@ -0,0 +1,62 @@
+﻿
+using UnityEngine;
+using TMPro;
+
+public class UIManager : MonoBehaviour
+{
+	static UIManager current;
+
+	public TextMeshProUGUI fearKilledText;			
+	public TextMeshProUGUI timeText;		
+	public TextMeshProUGUI deathText;		
+	public TextMeshProUGUI gameOverText;	
+
+
+	void Awake()
+	{
+		if (current != null && current != this)
+		{
+			Destroy(gameObject);
+			return;
+		}
+
+		current = this;
+		DontDestroyOnLoad(gameObject);
+	}
+
+
+	public static void Updatfear(int fearkilled)
+	{
+		if (current == null)
+			return;
+
+		current.fearKilledText.text = fearkilled.ToString();
+	}
+
+	public static void UpdateTimeUI(float time)
+	{
+		if (current == null)
+			return;
+
+		int minutes = (int)(time / 60);
+		float seconds = time % 60f;
+
+		current.timeText.text = minutes.ToString("00") + ":" + seconds.ToString("00");
+	}
+
+	public static void UpdateDeathUI(int deathCount)
+	{
+		if (current == null)
+			return;
+
+		current.deathText.text = deathCount.ToString();
+	}
+
+	public static void DisplayGameOverText()
+	{
+		if (current == null)
+			return;
+
+		current.gameOverText.enabled = true;
+	}
+}
