46781790caf3886da3d91f68de7b690a39f7d76e
Dhruv Agarwal <92450489+Dhruv127@users.noreply.github.com>
2024-02-10 20:16:10 +0530

    Update PlayerHealth.cs

--- PlayerHealth.cs
+++ PlayerHealth.cs
@@ -1,42 +1,44 @@
-﻿// This script handles detecting collisions with traps and telling the Game Manager
-// when the player dies
 
 using UnityEngine;
 
 public class PlayerHealth : MonoBehaviour
 {
-	public GameObject deathVFXPrefab;	//The visual effects for player death
-
-	bool isAlive = true;				//Stores the player's "alive" state
-	int trapsLayer;						//The layer the traps are on
-
-
+	public GameObject deathVFXPrefab;	
+	bool isAlive = true;				
+	int trapsLayer;
+	public PlayerMovement playerMovement;
+	
 	void Start()
 	{
-		//Get the integer representation of the "Traps" layer
 		trapsLayer = LayerMask.NameToLayer("Traps");
 	}
 
 	void OnTriggerEnter2D(Collider2D collision)
 	{
-		//If the collided object isn't on the Traps layer OR if the player isn't currently
-		//alive, exit. This is more efficient than string comparisons using Tags
 		if (collision.gameObject.layer != trapsLayer || !isAlive)
 			return;
-
-		//Trap was hit, so set the player's alive state to false
+		Debug.Log(playerMovement==null);
+		if(playerMovement!=null&&playerMovement.playerhasorb)
+		{
+			
+			Instantiate(deathVFXPrefab, transform.position, transform.rotation);
+			collision.gameObject.SetActive(false);
+			GameManager.PlayerWon();
+			return ;
+		}
 		isAlive = false;
 
-		//Instantiate the death particle effects prefab at player's location
+		if (collision.gameObject.tag != "air"&&(collision.gameObject.tag!="FinalBoss"))
 		Instantiate(deathVFXPrefab, transform.position, transform.rotation);
 
-		//Disable player game object
+		if(collision.gameObject.tag!="FinalBoss")
 		gameObject.SetActive(false);
 
-		//Tell the Game Manager that the player died and tell the Audio Manager to play
-		//the death audio
-		if(gameObject.tag!="Enemy")
+		if(gameObject.tag!="Enemy"&&(collision.gameObject.tag!="FinalBoss"))
 		GameManager.PlayerDied();
+		else
+		GameManager.fearkilled();
+
 		AudioManager.PlayDeathAudio();
 	}
 }
